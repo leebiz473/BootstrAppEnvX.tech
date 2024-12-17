3352a9ccbc948cd29313c6e63e1845aff4c6583f
Liam Nelson <lmjnelson@yahoo.com>
2024-12-17 23:20:46 +0000

    reformat config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 import defaultTheme from 'tailwindcss/defaultTheme';
 import forms from '@tailwindcss/forms';
-import { withTV } from 'tailwind-variants/transformer';
+import {withTV} from 'tailwind-variants/transformer';
 
 /** @type {import('tailwindcss').Config} */
 const config = withTV({
@@ -28,72 +28,72 @@ const config = withTV({
                 fg: 'hsl(var(--fg))',
                 primary: {
                     DEFAULT: 'hsl(var(--primary))',
-                        fg: 'hsl(var(--primary-fg))',
-                        50: 'hsl(204.7 99% 97%)',
-                        100: 'hsl(206.15 100% 92%)',
-                        200: 'hsl(206.57 99% 86%)',
-                        300: 'hsl(204.41 100% 77%)',
-                        400: 'hsl(206.02 100% 66%)',
-                        500: 'hsl(210.99 100% 57.99%)',
-                        600: 'hsl(215.79 98% 52%)',
-                        700: 'hsl(218.8 90% 48%)',
-                        800: 'hsl(220.35 84% 40%)',
-                        900: 'hsl(218.91 76% 33%)',
-                        950: 'hsl(220.26 68% 21%)'
-                        },
-                        secondary: {
-                            DEFAULT: 'hsl(var(--secondary))',
-                                fg: 'hsl(var(--secondary-fg))'
-                                },
-                                tertiary: {
-                                    DEFAULT: 'hsl(var(--tertiary))',
-                                        fg: 'hsl(var(--tertiary-fg))'
-                                        },
-                                        accent: {
-                                            DEFAULT: 'hsl(var(--accent))',
-                                                fg: 'hsl(var(--accent-fg))',
-                                                subtle: 'hsl(var(--accent-subtle))',
-                                                'subtle-fg': 'hsl(var(--accent-subtle-fg))'
-                                                },
-                                                success: {
-                                                    DEFAULT: 'hsl(var(--success))',
-                                                        fg: 'hsl(var(--success-fg))'
-                                                        },
-                                                        info: {
-                                                            DEFAULT: 'hsl(var(--info))',
-                                                                fg: 'hsl(var(--info-fg))'
-                                                                },
-                                                                danger: {
-                                                                    DEFAULT: 'hsl(var(--danger))',
-                                                                        fg: 'hsl(var(--danger-fg))'
-                                                                        },
-                                                                        warning: {
-                                                                            DEFAULT: 'hsl(var(--warning))',
-                                                                                fg: 'hsl(var(--warning-fg))'
-                                                                                },
-                                                                                muted: {
-                                                                                    DEFAULT: 'hsl(var(--muted))',
-                                                                                        fg: 'hsl(var(--muted-fg))'
-                                                                                        },
-                                                                                        overlay: {
-                                                                                            DEFAULT: 'hsl(var(--overlay))',
-                                                                                                fg: 'hsl(var(--overlay-fg))'
-                                                                                                }
-                                                                                                },
-                                                                                                borderRadius: {
-                                                                                                    '3xl': 'calc(var(--radius) + 7.5px)',
-                                                                                                    '2xl': 'calc(var(--radius) + 5px)',
-                                                                                                    xl: 'calc(var(--radius) + 2.5px)',
-                                                                                                    lg: 'calc(var(--radius))',
-                                                                                                    md: 'calc(var(--radius) - 2.5px)',
-                                                                                                    sm: 'calc(var(--radius) - 5px)'
-                                                                                                }
-                                                                                                }
-                                                                                                },
-                                                                                                plugins: [
-                                                                                                require('tailwindcss-animate'),
-                                                                                                require('tailwindcss-react-aria-components')],
-                                                                                                forms
-                                                                                                });
+                    fg: 'hsl(var(--primary-fg))',
+                    50: 'hsl(204.7 99% 97%)',
+                    100: 'hsl(206.15 100% 92%)',
+                    200: 'hsl(206.57 99% 86%)',
+                    300: 'hsl(204.41 100% 77%)',
+                    400: 'hsl(206.02 100% 66%)',
+                    500: 'hsl(210.99 100% 57.99%)',
+                    600: 'hsl(215.79 98% 52%)',
+                    700: 'hsl(218.8 90% 48%)',
+                    800: 'hsl(220.35 84% 40%)',
+                    900: 'hsl(218.91 76% 33%)',
+                    950: 'hsl(220.26 68% 21%)'
+                },
+                secondary: {
+                    DEFAULT: 'hsl(var(--secondary))',
+                    fg: 'hsl(var(--secondary-fg))'
+                },
+                tertiary: {
+                    DEFAULT: 'hsl(var(--tertiary))',
+                    fg: 'hsl(var(--tertiary-fg))'
+                },
+                accent: {
+                    DEFAULT: 'hsl(var(--accent))',
+                    fg: 'hsl(var(--accent-fg))',
+                    subtle: 'hsl(var(--accent-subtle))',
+                    'subtle-fg': 'hsl(var(--accent-subtle-fg))'
+                },
+                success: {
+                    DEFAULT: 'hsl(var(--success))',
+                    fg: 'hsl(var(--success-fg))'
+                },
+                info: {
+                    DEFAULT: 'hsl(var(--info))',
+                    fg: 'hsl(var(--info-fg))'
+                },
+                danger: {
+                    DEFAULT: 'hsl(var(--danger))',
+                    fg: 'hsl(var(--danger-fg))'
+                },
+                warning: {
+                    DEFAULT: 'hsl(var(--warning))',
+                    fg: 'hsl(var(--warning-fg))'
+                },
+                muted: {
+                    DEFAULT: 'hsl(var(--muted))',
+                    fg: 'hsl(var(--muted-fg))'
+                },
+                overlay: {
+                    DEFAULT: 'hsl(var(--overlay))',
+                    fg: 'hsl(var(--overlay-fg))'
+                }
+            },
+            borderRadius: {
+                '3xl': 'calc(var(--radius) + 7.5px)',
+                '2xl': 'calc(var(--radius) + 5px)',
+                xl: 'calc(var(--radius) + 2.5px)',
+                lg: 'calc(var(--radius))',
+                md: 'calc(var(--radius) - 2.5px)',
+                sm: 'calc(var(--radius) - 5px)'
+            }
+        }
+    },
+    plugins: [
+        require('tailwindcss-animate'),
+        require('tailwindcss-react-aria-components')],
+    forms
+});
 
-                                                                                                export default config;
+export default config;
