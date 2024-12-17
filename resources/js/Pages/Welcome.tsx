4a4837f20d1ebf9d9a6cbc780a73cb3b90e71348
Liam Nelson <lmjnelson@yahoo.com>
2024-12-17 14:39:57 +0000

    Fix PropType usage along with additional parameters
    The error “TS2315: Type PageProps is not generic”

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -1,7 +1,7 @@
 import { Link, Head } from '@inertiajs/react';
 import { PageProps } from '@/types';
 
-export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps<{ laravelVersion: string, phpVersion: string }>) {
+export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps & { laravelVersion: string; phpVersion: string }) {
     return (
         <>
             <Head title="Welcome" />
