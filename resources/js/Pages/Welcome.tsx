04c84e9d00370fec471c7159ee21ff8bc07d3614
Liam Nelson <lmjnelson@yahoo.com>
2024-12-18 08:01:59 +0000

    Add purple colour for darkmode

--- resources/js/Pages/Welcome.tsx
+++ resources/js/Pages/Welcome.tsx
@@ -46,7 +46,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps 
                             >
                                 <div>
                                     <div
-                                        className="h-16 w-16 bg-purple-100 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                        className="h-16 w-16 bg-purple-100 dark:bg-purple-800/20 flex items-center justify-center rounded-full">
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
@@ -93,7 +93,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps 
                                 className="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
                             >
                                 <div>
-                                    <div className="h-16 w-16 bg-purple-100 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                    <div className="h-16 w-16 bg-purple-100 dark:bg-purple-800/20 flex items-center justify-center rounded-full">
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
@@ -139,7 +139,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps 
                                 className="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500"
                             >
                                 <div>
-                                    <div className="h-16 w-16 bg-purple-100 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                    <div className="h-16 w-16 bg-purple-100 dark:bg-purple-800/20 flex items-center justify-center rounded-full">
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
@@ -183,7 +183,7 @@ export default function Welcome({ auth, laravelVersion, phpVersion }: PageProps 
 
                             <div className="scale-100 p-6 bg-white dark:bg-gray-800/50 dark:bg-gradient-to-bl from-gray-700/50 via-transparent dark:ring-1 dark:ring-inset dark:ring-white/5 rounded-lg shadow-2xl shadow-gray-500/20 dark:shadow-none flex motion-safe:hover:scale-[1.01] transition-all duration-250 focus:outline focus:outline-2 focus:outline-red-500">
                                 <div>
-                                    <div className="h-16 w-16 bg-purple-100 dark:bg-red-800/20 flex items-center justify-center rounded-full">
+                                    <div className="h-16 w-16 bg-purple-100 dark:bg-purple-800/20 flex items-center justify-center rounded-full">
                                         <svg
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
